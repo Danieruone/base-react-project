2f80d891d4f4fe8d57a4ed9ef0bb94ad5a071b79
Daniel <danieruone@gmail.com>
2023-03-03 23:12:09 -0500

    changue buttons position and label

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -9,20 +9,21 @@ export const Counter = () => {
   // redux
   const count = useSelector((state: RootState) => state.counter.value);
   const dispatch = useDispatch();
+
   return (
     <Container>
-      <button
-        aria-label='Increment value'
-        onClick={() => dispatch(increment())}
-      >
-        Increment
-      </button>
-      <span>{count}</span>
       <button
         aria-label='Decrement value'
         onClick={() => dispatch(decrement())}
       >
-        Decrement
+        -
+      </button>
+      <span>{count}</span>
+      <button
+        aria-label='Increment value'
+        onClick={() => dispatch(increment())}
+      >
+        +
       </button>
     </Container>
   );
