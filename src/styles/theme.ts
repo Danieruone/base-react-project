3486b8ef79dec561ec7f2fad6976b88d79c6d294
Daniel <danieruone@gmail.com>
2023-03-05 19:46:19 -0500

    Fix format

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -12,10 +12,12 @@ export const GlobalStyles = createGlobalStyle<{ theme: DefaultTheme }>`
     transition: background 0.2s ease-in, color 0.2s ease-in;
   }
 `;
+
 export const lightTheme = {
   body: '#f1f1f1',
   text: '#121620',
 };
+
 export const darkTheme = {
   body: '#121620',
   text: '#f1f1f1',
